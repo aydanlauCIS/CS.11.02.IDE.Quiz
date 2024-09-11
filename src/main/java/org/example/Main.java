7239a38e76abbb4b68f9162efa088b1160b8790e
Aydan Lau <aydanlau@gmail.com>
2024-09-11 12:37:47 +0800

    finished IDE quiz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public class Main {
-
     /** Method 1
      * Method name: circleArea
      * Method return type: double
@@ -9,7 +8,7 @@ public class Main {
      * Method signature: circleArea(int)
      * Returns the area of a circle. */
     public static double circleArea(int radius) {
-        return 0;
+        return Math.round(Math.PI * Math.pow(radius, 2) * 100.0) / 100.0;
     }
 
     /** Method 2
@@ -19,7 +18,7 @@ public class Main {
      * Method signature: circleCircum(int)
      * Returns the circumference of a circle. */
     public static double circleCircum(int radius) {
-        return 0;
+        return Math.round(2 * Math.PI * radius * 100.0) / 100.0;
     }
 
     /** Method 3
@@ -33,7 +32,9 @@ public class Main {
      * Circumference: 18.85
      * Area: 28.27 */
     public static String circleInfo(int radius) {
-        return null;
+        double circumference = circleCircum(radius);
+        double area = circleArea(radius);
+        return "Radius: " + radius + "\nCircumference: " + circumference + "\nArea: " + area;
     }
 
     /** Method 4
@@ -44,8 +45,9 @@ public class Main {
      * Returns a String detailing the number of hours and minutes.
      * Format of returned String: e.g. 3 hours and 46 minutes */
     public static String minutesToHoursMinutes(int minutes) {
-
-        return null;
+        int hours = minutes / 60;
+        int mins = minutes - hours * 60;
+        return hours + " hours and " + mins + " minutes";
     }
 
     /** Method 5
@@ -57,9 +59,9 @@ public class Main {
      * Format of returned String: e.g. 3:56 pm
      * */
     public static String twentyFourToTwelve(String time) {
-
-        return null;
-
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int new_hours = hours - 12;
+        return new_hours + time.substring(2, 5) + " pm";
     }
 
     /** Method 6
@@ -69,9 +71,7 @@ public class Main {
      * Method signature: distanceBetweenTwoPoints(int,int,int,int)
      * Returns the distance between two points */
     public static double distanceBetweenTwoPoints(int x1, int y1, int x2, int y2) {
-
-        return 0;
-
+        return Math.round(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) * 100.0) / 100.0;
     }
 
     /** Method 7
@@ -81,9 +81,7 @@ public class Main {
      * Method signature: fahrenheitToCelsius(int)
      * Returns the degrees celsius equivalent of the degrees in fahrenheit */
     public static double fahrenheitToCelsius(int fahrenheit) {
-
-        return 0;
-
+        return Math.round((fahrenheit - 32) * 5 / 9.0 * 100.0) / 100.0;
     }
 
     /** Method 8
@@ -93,9 +91,7 @@ public class Main {
      * Method signature: celsiusToFahrenheit(int)
      * Returns the degrees fahrenheit equivalent of the degrees in celsius */
     public static double celsiusToFahrenheit(int celsius) {
-
-        return 0;
-
+        return Math.round((celsius * 9.0 / 5.0 + 32.0) * 100.0) / 100.0;
     }
 
 }
